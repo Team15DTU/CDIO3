5c8e930d699fcfa5756561bc46819f9f79320289
Logstor <a.rottger_rydahl@live.dk>
2018-11-16 12:36:06 +0100

    Created TokenList class and Created constructors

---
+++ src/main/java/model/player/token/TokenList.java
@@ -0,0 +1,39 @@
+package model.player.token;
+
+import java.util.ArrayList;
+
+/**
+ * @author Alfred Röttger Rydahl
+ * @date 16-11-2018
+ */
+public class TokenList {
+
+    /*
+    ---------------------------------- Fields ----------------------------------
+     */
+    private ArrayList<Token> list;
+    
+    /*
+    ------------------------------ Constructors --------------------------------
+     */
+
+    /**
+     *
+     * @param list
+     */
+    public TokenList(ArrayList<Token> list) {
+        // Initialize
+        this.list = list;
+    }
+    /*
+    ------------------------------ Properties ----------------------------------
+     */
+    
+    /*
+    ---------------------------- Public Methods --------------------------------
+     */
+    
+    /*
+    ----------------------------- Support Methods ------------------------------
+     */
+}
