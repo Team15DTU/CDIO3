ea193c359ae8f219c96a857ae7f2bcae93f73a29
Logstor <a.rottger_rydahl@live.dk>
2018-11-20 18:42:22 +0100

    Created Constructors for the Controller.java

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,10 @@
 package controller;
 
+import model.player.Player;
+import view.gui.Gui;
+
+import java.util.ArrayList;
+
 /**
  * @author Alfred Röttger Rydahl
  */
@@ -10,9 +15,35 @@ public class Controller {
     --------------------------- Fields ---------------------------
      */
 
+    private Gui gui;
+
     /*
     ------------------------ Constructors ------------------------
      */
+
+    /**
+     * Use this constructor if you don't know the players
+     * in the game
+     * @param fields An Array of Field's
+     */
+    public Controller ( Field[] fields ) {
+
+        // Create the Gui
+        gui = new Gui( fields );
+    }
+
+    /**
+     * Use this controller if you already know the players
+     * in the game.
+     * @param players An ArrayList<Player></Player>
+     * @param fields An Array of Field's
+     */
+    public Controller ( ArrayList<Player> players, Field[] fields ) {
+
+        // Create the new Gui
+        gui = new Gui(players, fields);
+    }
+
     /*
     ------------------------- Properties -------------------------
      */
