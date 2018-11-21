0111b2b9dbf4386e6b9bde64710251f5342adfde
Logstor <a.rottger_rydahl@live.dk>
2018-11-21 13:00:32 +0100

    Code folding

--- src/main/java/model/board/Field.java
+++ src/main/java/model/board/Field.java
@@ -10,42 +10,44 @@ public abstract class Field {
     -------------------------- Fields --------------------------
      */
 
-        protected int fieldNumber;
-        protected String title;
-        protected String description;
-        protected Player owner = null;
+    protected int fieldNumber;
+    protected String title;
+    protected String description;
+    protected Player owner;
 
     /*
     ----------------------- Constructor -------------------------
      */
 
-        protected Field (int fieldNumber, String title, String description) {
+    protected Field (int fieldNumber, String title, String description) {
 
-            this.fieldNumber=fieldNumber;
-            this.title=title;
-            this.description=description;
+        this.fieldNumber=fieldNumber;
+        this.title=title;
+        this.description=description;
 
-        }
+    }
 
     /*
     ------------------------ Properties -------------------------
      */
 
+    //<editor-fold desc="Properties">
     public int getCardNumber() {return fieldNumber;}
 
-    public void setCardNumber(int fieldNumber) {this.fieldNumber = fieldNumber;}
+    public void setCardNumber (int fieldNumber) {this.fieldNumber = fieldNumber;}
 
     public String getTitle() {        return title;    }
 
-    public void setTitle(String title) {        this.title = title;    }
+    public void setTitle (String title) {        this.title = title;    }
 
     public String getDescription() {        return description;    }
 
-    public void setDescription(String description) {        this.description = description;    }
+    public void setDescription (String description) {        this.description = description;    }
 
     public Player getOwner() {        return owner;    }
 
-    public void setOwner(Player owner) {        this.owner = owner;    }
+    public void setOwner (Player owner) {        this.owner = owner;    }
+    //</editor-fold>
 
     /*
     ---------------------- Public Methods -----------------------
