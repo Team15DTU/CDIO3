0838403117be6518e83dc2a71bb6121145b6ea17
Karl Jeppesen <32724999+KarlJeppesen@users.noreply.github.com>
2018-11-21 09:43:27 +0100

    Created fields and board

---
+++ src/main/java/model/board/Field.java
@@ -0,0 +1,41 @@
+package model.board;
+
+public class Field {
+
+    /*
+    -------------------------- Fields --------------------------
+     */
+
+        private int fieldNumber;
+
+    /*
+    ----------------------- Constructor -------------------------
+     */
+
+        public Field (int fieldNumber) {
+
+            this.fieldNumber=fieldNumber;
+
+        }
+
+    /*
+    ------------------------ Properties -------------------------
+     */
+
+        public int getCardNumber() {return fieldNumber;}
+
+        public void setCardNumber(int cardNumber) {this.fieldNumber = fieldNumber;}
+
+    /*
+    ---------------------- Public Methods -----------------------
+     */
+
+
+
+    /*
+    ---------------------- Support Methods ----------------------
+     */
+
+
+}
+
