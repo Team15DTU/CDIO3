31e8b27a3b23da7e06243451e743d7c49ae68a75
Logstor <a.rottger_rydahl@live.dk>
2018-11-21 13:14:03 +0100

    Code folding and some formatting

--- src/main/java/model/chancecard/Card.java
+++ src/main/java/model/chancecard/Card.java
@@ -32,18 +32,19 @@ public abstract class Card {
     /*
     ------------------------ Properties -------------------------
      */
+    //<editor-fold desc="Properties">
+    public int getCardNumber()                      { return cardNumber; }
 
-    public int getCardNumber() { return cardNumber; }
+    public void setCardNumber(int cardNumber)       { this.cardNumber = cardNumber; }
 
-    public void setCardNumber(int cardNumber) { this.cardNumber = cardNumber; }
+    public String getText()                         { return text; }
 
-    public String getText() { return text; }
+    public void setText(String text)                { this.text = text; }
 
-    public void setText(String text) { this.text = text; }
+    public String getDescription()                  { return description; }
 
-    public String getDescription() { return description; }
-
-    public void setDescription(String description) { this.description = description; }
+    public void setDescription(String description)  { this.description = description; }
+    //</editor-fold>
 
     /*
     ---------------------- Public Methods -----------------------
