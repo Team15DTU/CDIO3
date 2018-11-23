d0b16b7f2b045e46d0d78d2baa80ac09011a313d
aksina <saaruppoulsen@gmail.com>
2018-11-23 12:28:33 +0100

    Transfer testene er done
    Transfer test er done

---
+++ src/test/java/model/chancecard/cards/TransferTest.java
@@ -0,0 +1,37 @@
+package model.chancecard.cards;
+
+import model.player.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransferTest {
+
+
+    @Test
+    public void getMoneyTransferred() {
+        Transfer transfer = new Transfer(0,"lol","lol",5);
+
+        assertEquals(5,transfer.getMoneyTransferred());
+    }
+
+    @Test
+    public void setMoneyTransferred() {
+        Transfer transfer = new Transfer(0,"lol","lol",2);
+        transfer.setMoneyTransferred(400);
+        assertEquals(400,transfer.getMoneyTransferred());
+    }
+
+    @Test
+    public void action() {
+
+        Player player = new Player("jens",200,1);
+        int money = player.getAccount().getBalance();
+        Transfer transer = new Transfer(0, "hej", "lol", 200);
+
+        transer.action(player);
+        assertEquals(400, player.getAccount().getBalance());
+
+
+    }
+}
