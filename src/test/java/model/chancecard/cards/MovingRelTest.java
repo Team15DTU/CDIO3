7d0895e5aee4e12a63c6bd9b85dfca12819b3bd0
aksina <saaruppoulsen@gmail.com>
2018-11-26 16:21:55 +0100

    update i movingRelTest og AccountTest

--- src/test/java/model/chancecard/cards/MovingRelTest.java
+++ src/test/java/model/chancecard/cards/MovingRelTest.java
@@ -10,9 +10,9 @@ public class MovingRelTest {
     @Test
     public void action() {
         MovingRel movingRel = new MovingRel("Does It move","If it move the test is complete",4);
-        Player player = new Player("jens",100,1);
+        Player player = new Player("jens",100,3);
         movingRel.action(player);
 
-        assertEquals(5,player.getPosition());
+        assertEquals(7,player.getPosition());
     }
 }
