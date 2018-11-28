206792fff91a48dfad3106f7d97f224ef9d6e7bc
aksina <saaruppoulsen@gmail.com>
2018-11-28 10:30:11 +0100

    Create PrisonTest.java
    Prison test er lavet!
    codecoverage:
    60% af klasserne
    35% af metoderne
    30% af linjerne

---
+++ src/test/java/model/board/fields/PrisonTest.java
@@ -0,0 +1,22 @@
+package model.board.fields;
+
+import model.board.Field;
+import model.player.Player;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class PrisonTest {
+
+    @Test
+    public void action() {
+
+        Field field = new Prison(1,"32","chancefelt",0, Color.red);
+        Player player = new Player("jens",100,1);
+        field.action(player);
+        assertEquals(7,player.getPosition());
+
+    }
+}
